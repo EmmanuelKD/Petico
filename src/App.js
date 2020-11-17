980ea4b96168b8b8b9dcd8720464a18aefaef4c9
emmanuel kofi <emmanuelkofi@emmanuels-MacBook-Pro.local>
2020-11-17 18:06:36 +0000

    first change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+guyz if you see this you have successful clone Zaire
         </a>
       </header>
     </div>
